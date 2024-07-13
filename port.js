7ff9caad72e80646a2a10096cbde409bc2221a98
AYUSH KATIYAR <57161355+ayush75-hub@users.noreply.github.com>
2024-07-13 19:50:43 +0530

    Add files via upload

---
+++ port.js
@@ -0,0 +1,12 @@
+const list = document.querySelectorAll(".list");
+function activeLink() {
+  list.forEach((item) => item.classList.remove("active"));
+  this.classList.add("active");
+}
+list.forEach((item) => item.addEventListener("click", activeLink));
+
+const page1 = document.querySelector(".page1");
+const page2 = document.querySelector(".page2");
+const page3 = document.querySelector(".page3");
+const page4 = document.querySelector(".page4");
+const page5 = document.querySelector(".page5");
